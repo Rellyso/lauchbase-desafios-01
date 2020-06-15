3284f7bd4ed67ed88040bc4d3f539098155c58c6
Rellyson <rellysondouglas2015@gmail.com>
2020-06-15 20:19:31 -0300

    Cálculo de aposentadoria

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const nome = 'Carlos'
-const peso = 84
-const altura = 1.88
+const nome = 'Silvana'
+const sexo = 'F'
+const idade = 48
+const contribuicao = 30
 
-const imc = peso / (altura * altura)
-
-if (imc >= 30) {
-    console.log(`${nome} você está acima do peso`)
+if (sexo == 'M') {
+    if (contribuicao >= 35 && (contribuicao + idade) >= 95) {
+        console.log(`${nome}, você pode se aposentar!`)
+    } else {
+        console.log(`${nome}, você ainda não pode se aposentar!`)
+    }
+} else if (sexo == 'F') {
+    if (contribuicao >= 30 && (contribuicao + idade) >= 85) {
+        console.log(`${nome}, você pode se aposentar!`)
+    } else {
+        console.log(`${nome}, você ainda não pode se aposentar!`)
+    }
 } else {
-    console.log(`${nome} você não está acima do peso`)
+    console.log('Dados incorretos')
 }
+
+// Se homem, tempo de contribuição mínimo == 35
+
+
+// Se mulher, contribuicao >= 30
