80f89d85763047c2229b44c641e2baf8146b82a9
Rellyson <rellysondouglas2015@gmail.com>
2020-06-15 20:22:15 -0300

    Apresentacao

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const nome = 'Silvana'
-const sexo = 'F'
-const idade = 48
-const contribuicao = 30
+const welcome = 'Hello World'
 
-if (sexo == 'M') {
-    if (contribuicao >= 35 && (contribuicao + idade) >= 95) {
-        console.log(`${nome}, você pode se aposentar!`)
-    } else {
-        console.log(`${nome}, você ainda não pode se aposentar!`)
-    }
-} else if (sexo == 'F') {
-    if (contribuicao >= 30 && (contribuicao + idade) >= 85) {
-        console.log(`${nome}, você pode se aposentar!`)
-    } else {
-        console.log(`${nome}, você ainda não pode se aposentar!`)
-    }
-} else {
-    console.log('Dados incorretos')
-}
-
-// Se homem, tempo de contribuição mínimo == 35
-
-
-// Se mulher, contribuicao >= 30
+console.log(welcome)
